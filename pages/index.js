3a67ebf48dd5e7e6da6aaa4b6fc001ebf61fa978
Júlia Küchle <juliagkuchle@gmail.com>
2026-04-29 00:45:16 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Sugestão de <a href="https://www.amazon.com.br/PHILIPS-Microfone-TAUE101BK-00-intra-auricular/dp/B084S6BCJN?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2DLNAX65VYFRF&dib=eyJ2IjoiMSJ9.KNj0wEYguAs6uqAEd00akLBmJ9TO2yU_O21mYKE_AbEJQUqnWAxxh7oIuY3psoc7O6C-qY5sQmtRw4m4bk6vwhZchvQH4iBFcudN6X_r2DvHweARkuwjF_FxjWQO-5DPFi4yQ4EJooni4YFvKcrOY-iMqHSLPW0XP70aK2Ks4rzRONWPcm7fX4vGI2LUXbEjDPz0oF196OWLFxhoRUHbsViv_2mS04poFjh9KwxUyJ9i6Tl4BoSemxzFLljwByvvQwR875iI5YSiGK4bREJvtXQZst19-OsBFYZMFn65_j8.McYE2bXh0DVyieAlc_Ndd16gnL6UvwlsPPS0KeiMlLc&dib_tag=se&keywords=fone+de+ouvido&qid=1777172730&sprefix=fone+de+ouvi%2Caps%2C242&sr=8-5">fone</a></h1>
+  return (
+    <h1>
+      Sugestão de{" "}
+      <a href="https://www.amazon.com.br/PHILIPS-Microfone-TAUE101BK-00-intra-auricular/dp/B084S6BCJN?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2DLNAX65VYFRF&dib=eyJ2IjoiMSJ9.KNj0wEYguAs6uqAEd00akLBmJ9TO2yU_O21mYKE_AbEJQUqnWAxxh7oIuY3psoc7O6C-qY5sQmtRw4m4bk6vwhZchvQH4iBFcudN6X_r2DvHweARkuwjF_FxjWQO-5DPFi4yQ4EJooni4YFvKcrOY-iMqHSLPW0XP70aK2Ks4rzRONWPcm7fX4vGI2LUXbEjDPz0oF196OWLFxhoRUHbsViv_2mS04poFjh9KwxUyJ9i6Tl4BoSemxzFLljwByvvQwR875iI5YSiGK4bREJvtXQZst19-OsBFYZMFn65_j8.McYE2bXh0DVyieAlc_Ndd16gnL6UvwlsPPS0KeiMlLc&dib_tag=se&keywords=fone+de+ouvido&qid=1777172730&sprefix=fone+de+ouvi%2Caps%2C242&sr=8-5">
+        fone
+      </a>
+    </h1>
+  );
 }
 
 export default Home;
