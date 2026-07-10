84b266280ba624fca61bcf6e5f50ed6d73ae5410
Júlia Küchle <juliagkuchle@gmail.com>
2026-07-10 01:00:09 +0000

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,65 @@
+import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import { ServiceError } from "infra/errors.js";
+
+const defaultMigrationsOptions = {
+  databaseUrl: process.env.DATABASE_URL,
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dbClient: dbClient,
+    });
+
+    return pendingMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      message: "Erro no migrationRunner.",
+      cause: error,
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dbClient: dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      message: "Erro no migrationRunner.",
+      cause: error,
+    });
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
